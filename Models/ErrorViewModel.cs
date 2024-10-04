d8b7b08bbd9b33b2f6586ab7c81868ec0dbc90e8
admin <admin@PC13>
2024-10-04 15:04:40 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheWayShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
