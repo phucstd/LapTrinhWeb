8001c56eb4990a826b254999420fc8ad1a40b595
unknown <phuc.nm01953@sinhvien.hoasen.edu.vn>
2024-10-18 16:44:51 +0700

    Tuan4
    + add database

---
+++ AppData/AppDBContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using TheWayShop.Models;
+
+namespace TheWayShop.AppData
+{
+    public class AppDBContext : IdentityDbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options)
+           : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+        }
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<OrderProduct> OrderProducts { get; set; } = null!;
+    }
+   
+
+   
+
+    
+
+    
+}
---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace TheWayShop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        public string? ImagePath { get; set; }
+
+        public int? CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public ICollection<OrderProduct>? OrderProducts { get; set; }
+    }
+}
