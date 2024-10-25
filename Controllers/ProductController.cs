24aaf125bea4b68daf58f7a9ace14814c4943d50
unknown <phuc.nm01953@sinhvien.hoasen.edu.vn>
2024-10-25 16:42:45 +0700

    + add database

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheWayShop.AppData;
+using TheWayShop.Models;
+using System.Linq;
+
+namespace TheWayShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDBContext _db;
+        public ProductController(AppDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Detail(int id)
+        {
+            Product p = _db.Products.Where(_ => _.Id == id).FirstOrDefault();
+          
+            return View(p);
+        }
+        public IActionResult ProductList(int id)
+        {
+            List<Product> list = _db.Products.Where(_ => _.CategoryId == id).ToList();
+
+            return View(list);
+        }
+    }
+}
