5855e3118e0df78f7280e2ec3280408b9e5c73b8
phuc.nor <phuc.nor@pentoro.io>
2024-12-15 19:26:47 +0700

    + add component for category cat box

---
+++ Components/CategoryCatBoxViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TheWayShop.AppData;
+
+namespace TheWayShop.Components
+{
+    public class CategoryCatBoxViewComponent : ViewComponent
+    {
+        private readonly AppDBContext _dbContext;
+
+        public CategoryCatBoxViewComponent(AppDBContext db)
+        {
+            _dbContext = db;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var categories = await _dbContext.Categories.ToListAsync();
+            return View(categories);
+        }
+    }
+}
---
+++ Migrations/20241215115543_add-image-for-category.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheWayShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class addimageforcategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Categories");
+        }
+    }
+}
